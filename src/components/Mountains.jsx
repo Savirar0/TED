fdf20abdf7e2b7167b4afb777f7f4e0234a11cf4
Navadeep <navdeep2666@gmail.com>
2025-03-23 13:11:55 +0530

    import error

--- src/components/Mountains.jsx
+++ src/components/Mountains.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import "../styles/Mountains.css";
+import "/src/styles/Mountains.css";
 import SocialIcon from "../utils/SocialIcon";
 import logo from "../assets/logos/ILLUMINAT.png";
 
