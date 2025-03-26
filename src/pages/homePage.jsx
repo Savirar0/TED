c71165af181e3a173d62038268f053c1cc67111b
Navadeep <navdeep2666@gmail.com>
2025-03-26 20:58:49 +0530

    .

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 {/*import TEDxLanding from "../components/TEDxLanding";*/}
 import WhatTEDx from "../components/WhatTEDx";
 import CollegeImageGallerySection from "../components/CollegeImageGallerySection";
+import MountainSection from "../components/Mountains";
 
 const Homepage = () => {
     return (
       <div className="h-full bg-black">
         {/*<TEDxLanding />*/}
+        <MountainSection />
         <WhatTEDx />
         <CollegeImageGallerySection />
       </div>
